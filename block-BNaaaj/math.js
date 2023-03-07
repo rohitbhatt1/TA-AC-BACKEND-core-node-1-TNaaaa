8914350c2c7df1be5f2b4e3e8a106206f49a25a5
rohitbhatt1 <rohitsharma80449@gmail.com>
2023-03-07 11:31:05 +0530

    add

---
+++ block-BNaaaj/math.js
@@ -0,0 +1,21 @@
+// function  sum(a,b){
+//     return a+b;
+// }
+
+// function  square(a){
+//     return a*a;
+// }
+// var constants = {
+//     PI:3.14,
+//     sin0:0,
+//     cos0:1
+// }
+ 
+module.exports ={
+    sum:(a,b) =>{
+        return a + b;
+    },
+    square:(a) =>{
+        return a*a;
+    }
+}
